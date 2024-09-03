8e9bc54a224ef2f05a68755709948abb9b93538a
brMonteiro-G <brgabriel.monteiro@gmail.com>
2024-09-02 21:50:48 -0300

    fix: solve bean issue

--- src/main/java/com/spring/jwt/validator/configuration/local/DynamoDBConfigLocal.java
+++ src/main/java/com/spring/jwt/validator/configuration/local/DynamoDBConfigLocal.java
@@ -3,17 +3,17 @@ package com.spring.jwt.validator.configuration.local;
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.auth.BasicSessionCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
-import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("local")
 public class DynamoDBConfigLocal {
 
     @Value("${aws.dynamodb.endpoint}")
@@ -46,16 +46,8 @@ public class DynamoDBConfigLocal {
                                 awsRegion
                         )
                 )
-                .withCredentials(
-                        new AWSStaticCredentialsProvider(
-                                new BasicSessionCredentials(
-                                        amazonAWSAccessKey,
-                                        amazonAWSSecretKey,
-                                        amazonAWSSessionToken
-                                )
-                        )
-                )
-                .build();
+                .withCredentials(new AWSStaticCredentialsProvider(
+                        new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey))).build();
     }
 
 }
